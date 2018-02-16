30466f79b0a081ca1cd1e4e9e8fc23ffeb662a90
Postaron <florent.denef@gmail.com>
2018-02-16 09:44:50 +0100

    Ajout de nouveau type pour me faciliter la tâche. Modification du main.

--- main.c
+++ main.c
@@ -5,32 +5,29 @@
 #include <fcntl.h>
 #include <errno.h>
 
+/* file_path x y */
 int main(int argc, char *argv[])
 {
-    int fd;
-    long int size;
-    if (argc != 2)
-    {
-        fprintf(stderr, "Pas assez d'argument : un nom de fichier est nécessaire.\n");
-        exit(EXIT_FAILURE);
-    }
-    if ((fd = open(argv[1], O_RDONLY, S_IRUSR)) == -1)
-    {
-        perror("Erreur ouverture du fichier ");
-        exit(EXIT_FAILURE);
-    }
-    printf("Le fichier existe !\n");
-    if ((size = lseek(fd, 0, SEEK_END)) == -1)
-    {
-        perror("Erreur de parcours de fichier ");
-        exit(EXIT_FAILURE);
-    }
-    printf("Taille du fichier : %ld\n", size);
-    if (close(fd) == -1)
-    {
-        perror("Erreur lors de la fermeture du fichier ");
-        exit(EXIT_FAILURE);
-    }
-    printf("Fichier fermé.\n");
-    return EXIT_SUCCESS;
+	int fd;
+	int x, y;
+	if (argc != 3)
+	{
+		fprintf(stderr,
+				"Pas assez d'argument : un nom de fichier est nécessaire.\n");
+		exit(EXIT_FAILURE);
+	}
+	x = atoi(argv[2]), y = atoi(argv[3]);
+	if ((fd = open(argv[1], O_RDWR | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR))
+			== -1)
+	{
+		perror("Erreur ouverture du fichier ");
+		exit(EXIT_FAILURE);
+	}
+	if (close(fd) == -1)
+	{
+		perror("Erreur lors de la fermeture du fichier ");
+		exit(EXIT_FAILURE);
+	}
+	printf("Fichier fermé.\n");
+	return EXIT_SUCCESS;
 }
---
+++ map.h
@@ -0,0 +1,10 @@
+#ifndef MAP_H_
+#define MAP_H_
+
+typedef struct{
+	int** map;
+	int sizeX;
+	int sizeY;
+} map_t;
+
+#endif /* MAP_H_ */
